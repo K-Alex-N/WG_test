a64fa2cd08b334bdf0604b85d606dac046972e68
aleksei <KurochkinAlexei@yandex.ru>
2025-10-10 14:35:48 +0200

    init

--- conftest.py
+++ conftest.py
@@ -31,40 +31,48 @@ def get_random_component(component):
         "engine": ENGINES_COUNT
     }.get(component)
 
-    return f"{component}-{random.randint(1, max_for_component)}"
+    return f"{component.capitalize()}-{random.randint(1, max_for_component)}"
 
 
-def randomize_ships(cursor):
+def randomize_ships(cursor, conn):
     ships = cursor.execute("SELECT * FROM ships").fetchall()
 
     for ship_id, *data in ships:
+        # print()
+        # print(ship_id)
+        # print(*data)
         component = random.choice(["weapon", "hull", "engine"])
 
         new_component = get_random_component(component)
+        # print(new_component)
+        sql = f"""
+            UPDATE ships 
+            SET {component} = "{new_component}"
+            WHERE ship = "{ship_id}";
+        """
+        # print(sql)
+        cursor.execute(sql)
+        conn.commit()
 
-        cursor.execute(f"UPDATE ships "
-                       f"SET {component} = {new_component} "
-                       f"WHERE ship = {ship_id}")
+    # component_db = component_to_db[component]
+    # cursor.execute(f"UPDATE {component_db} SET {param} = ? WHERE weapon = ?"
+    #
+    # pass
 
-        # component_db = component_to_db[component]
-        # cursor.execute(f"UPDATE {component_db} SET {param} = ? WHERE weapon = ?"
-        #
-        # pass
+    # pytest.fail(e) использовать
 
-        # pytest.fail(e) использовать
+    # может быть создать ДАТАКЛАССЫ или МОДЕЛЬКИ для каждой БД.
+    # ЧТОБЫ потом через точку доставать нужный параметр!!!!
+    #
+    # можно валидацию через Пайдентик сделать
 
-        # может быть создать ДАТАКЛАССЫ или МОДЕЛЬКИ для каждой БД.
-        # ЧТОБЫ потом через точку доставать нужный параметр!!!!
-        #
-        # можно валидацию через Пайдентик сделать
+    # def randomize_components():
+    #     # randomize hulls
+    #     # randomize hulls
+    #     # randomize hulls
+    #     pass
 
-        # def randomize_components():
-        #     # randomize hulls
-        #     # randomize hulls
-        #     # randomize hulls
-        #     pass
-
-        # @ pytest.fixture(scope='session')
+    # @ pytest.fixture(scope='session')
 
 
 @pytest.fixture(scope='session')
@@ -74,5 +82,5 @@ def changed_db():
     with conn_db(tmp_db_name) as conn:
         cursor = conn.cursor()
 
-        randomize_ships(cursor)
+        randomize_ships(cursor, conn)
         # randomize_components(cursor)
